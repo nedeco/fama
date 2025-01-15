4fdfb0e6ad084e77a3d0256c1e9d328df5880c6e
n.brendt <n.brendt@nedeco.de>
2025-01-15 09:12:47 +0100

    feat: added kotlin linter and GitHub action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -47,30 +49,12 @@ tasks.jar {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
-tasks.named("compileKotlin") {
-    dependsOn("generateBuildConfig")
-}
-
-task("generateBuildConfig") {
-    fun getEnv(key: String): String = System.getenv(key)?.let { "\"$it\"" } ?: "TODO(\"Missing env variable $key in build.\")"
-
-    val outputDir = file("build/generated/src/main/kotlin/de/osca/fama/generated")
-    doLast {
-        outputDir.mkdirs()
-        file("${outputDir.path}/BuildConfig.kt").writeText(
-            """
-            package de.osca.fama.generated
-
-            object BuildConfig {
-                const val VERSION: String = "${project.version}"
-                val SUPPORT_URL: String = ${getEnv("SUPPORT_URL")}
-                val RABBIT_MQ_STOMP_URL: String = ${getEnv("RABBIT_MQ_STOMP_URL")}
-                val RABBIT_MQ_STOMP_USERNAME: String = ${getEnv("RABBIT_MQ_STOMP_USERNAME")}
-                val RABBIT_MQ_STOMP_PASSWORD: String = ${getEnv("RABBIT_MQ_STOMP_PASSWORD")}
-                val SENTRY_DSN: String = ${getEnv("SENTRY_DSN")}
-            }
-            
-            """.trimIndent()
-        )
+configure<KtlintExtension> {
+    outputToConsole.set(true)
+    outputColorName.set("RED")
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
     }
 }
+
+apply("buildconfig.gradle.kts")
