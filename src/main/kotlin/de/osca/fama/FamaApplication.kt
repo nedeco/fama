60af8462122fc8ea6954d6febd01be7c67312c85
n.brendt <n.brendt@nedeco.de>
2025-01-23 18:38:56 +0100

    feat: move error handling from main to fama application

--- src/main/kotlin/de/osca/fama/FamaApplication.kt
+++ src/main/kotlin/de/osca/fama/FamaApplication.kt
@@ -1,5 +1,6 @@
 package de.osca.fama
 
+import co.touchlab.kermit.Logger
 import de.osca.fama.digitaltwin.TwinMessageManager
 import de.osca.fama.logger.logger
 import de.osca.fama.mqtt.MqttManager
@@ -8,24 +9,83 @@ import de.osca.fama.settings.Settings
 import de.osca.fama.smarthomeadapter.SmartHomeAdapter
 import io.sentry.Sentry
 import io.sentry.kotlin.SentryContext
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-object FamaApplication: KoinComponent {
-    private val mqttManager: MqttManager by inject()
-    val settings: Settings by inject()
+object FamaApplication : KoinComponent {
+    private val settings: Settings by inject()
     private val twinMessageManager: TwinMessageManager by inject()
     private val smartHomeAdapter: SmartHomeAdapter by inject()
     private val mqttManager: MqttManager by inject()
     private val logger by logger()
 
-    init {
+    @OptIn(DelicateCoroutinesApi::class)
+    fun launch() = runBlocking {
         logger.i { "Start" }
         if (settings.enableSentry && settings.SENTRY_DSN !is Nothing) {
             setupSentry()
+        }
+
+        val job =
+            GlobalScope.async {
+                start()
+            }
+
+        runBlocking(SentryContext()) {
+            try {
+                job.await()
+            } catch (e: Throwable) {
+                handleError(e)
+            }
+        }
+    }
+
+    @Throws(EnvVarMissingException::class)
+    private suspend fun start() = withContext(Dispatchers.Default + SentryContext()) {
+        if (smartHomeAdapter.mqttEnabled) {
+            mqttManager.start()
+        }
+
+        twinMessageManager.start()
+
+        val twinMessage =
+            async {
+                if (settings.enableSensorStation) {
+                    twinMessageManager.listenSensors()
+                }
+            }
+
+        twinMessage.await()
+
+        logger.i { "Shutdown" }
+        twinMessageManager.stop()
+
+        if (smartHomeAdapter.mqttEnabled) {
+            mqttManager.stop()
+        }
+    }
+
+    private fun handleError(error: Throwable) {
+        when (val cause = error.cause) {
+            is EnvVarMissingException -> {
+                cause.message?.let {
+                    Logger.e(it, tag = "ENV")
+                }
+            }
+            else -> {
+                if (FamaApplication.settings.debug) {
+                    Logger.e("Unexpected Error -", error)
+                } else {
+                    Logger.e("Unexpected Error - ${error.cause?.message}")
+                }
+                Sentry.captureException(error)
+            }
         }
     }
 
@@ -50,30 +110,5 @@ object FamaApplication: KoinComponent {
             )
         }
         logger.i("Sentry is enabled")
-    }
-
-    @Throws(EnvVarMissingException::class)
-    suspend fun start() = withContext(Dispatchers.Default + SentryContext()) {
-        logger.i { "Start Fama" }
-        if (smartHomeAdapter.mqttEnabled) {
-            mqttManager.start()
-        }
-        twinMessageManager.start()
-
-        val twinMessage =
-            async {
-                if (settings.enableSensorStation) {
-                    twinMessageManager.listenSensors()
-                }
-            }
-
-        twinMessage.await()
-
-        logger.i { "Shutdown" }
-        twinMessageManager.stop()
-
-        if (smartHomeAdapter.mqttEnabled) {
-            mqttManager.stop()
-        }
     }
 }
